1a717d970012df47ca556805105448a14b49471c
tetuaoro <65575727+tetuaoro@users.noreply.github.com>
2024-11-15 16:58:48 +0100

    update: factorize the code

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,39 @@
 #[cfg(feature = "server")]
 #[tokio::main]
 async fn main() {
-    use leptos::logging;
-    use rupa::cli::*;
-
-    let cli = AppCli::parse();
-
-    match cli.mode {
-        Modes::Master => {
-            logging::log!("Run in mode master");
-            run_as_master().await
-        }
-        Modes::Edge => {
-            logging::log!("Run in mode edge");
-            run_as_edge().await
-        }
-    };
-}
-
-#[cfg(feature = "server")]
-async fn run_as_master() {
     use axum::Router;
+    use leptos::logging;
     use leptos::prelude::*;
-    use leptos::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
     use rupa::app::*;
+    use rupa::cli::*;
 
-    let conf = get_configuration(None).unwrap();
+    let conf = get_configuration(None).expect("couldn't get leptos config");
     let leptos_options = conf.leptos_options;
     let addr = leptos_options.site_addr;
     let routes = generate_route_list(App);
 
-    // build our application with a route
-    let app = Router::new()
-        .leptos_routes(&leptos_options, routes, {
-            let leptos_options = leptos_options.clone();
-            move || shell(leptos_options.clone())
-        })
-        .fallback(leptos_axum::file_and_error_handler(shell))
-        .with_state(leptos_options);
+    // dbg!(server_fn::axum::server_fn_paths().collect::<Vec<_>>());
 
-    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    let app = match AppCli::parse().mode {
+        Modes::Master => Router::new()
+            .leptos_routes(&leptos_options, routes, {
+                let leptos_options = leptos_options.clone();
+                move || shell(leptos_options.clone())
+            })
+            .fallback(leptos_axum::file_and_error_handler(shell))
+            .with_state(leptos_options),
+        Modes::Edge => Router::new().route(
+            "/*fn_name",
+            axum::routing::any(leptos_axum::handle_server_fns).with_state(leptos_options),
+        ),
+    };
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.expect("couldn't bind the address provided");
     logging::log!("listening on http://{}", &addr);
     axum::serve(listener, app.into_make_service())
         .await
-        .unwrap();
-}
-
-#[cfg(feature = "server")]
-async fn run_as_edge() {
-    use axum::Router;
-    use leptos::prelude::*;
-    use leptos::*;
-
-    let conf = get_configuration(None).unwrap();
-    let leptos_options = conf.leptos_options;
-    let addr = leptos_options.site_addr;
-
-    // build our application with a route
-    let app = Router::new().route(
-        "/api/*fn_name",
-        axum::routing::post(leptos_axum::handle_server_fns),
-    );
-
-    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    logging::log!("listening on http://{}", &addr);
-    axum::serve(listener, app.into_make_service())
-        .await
-        .unwrap();
+        .expect("couldn't serve the app");
 }
 
 #[cfg(feature = "server")]
